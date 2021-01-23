8c1fd81e346d8e8cc535b3a538575487c721028b
aaron_tetracube <aaronpsam@gmail.com>
2021-01-23 22:19:57 +0530

    type coercion

--- typeconversion.js
+++ typeconversion.js
@@ -1,5 +1,16 @@
+// type convertion
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(inputYear + 18);
 console.log(Number(inputYear) + 18);
 console.log("type Conversion");
+console.log(Number('Aaron'));
+console.log(String(23),23);
+
+// type coercion
+
+console.log("I am "+23+"years Old");
+console.log("23"-"10"-3);
+console.log("23"+"10"+3);
+console.log("23" * 2);
+
