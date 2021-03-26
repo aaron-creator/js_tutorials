9033c4ba6251023817bc8b5391debf76228beaf5
aaron_tetracube <aaronpsam@gmail.com>
2021-03-26 23:58:36 +0530

    updated.

--- boolean.js
+++ boolean.js
@@ -3,3 +3,12 @@ console.log(Boolean(undefined));
 console.log(Boolean("Aaron"));
 console.log(Boolean({}));
 console.log(Boolean(""));
+
+
+const money = 10;
+if(money){
+    console.log("Don't Spend it all");
+}
+else{
+    console.log("You should get a job!");
+}
