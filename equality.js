c77980d0198d29817ff118e426d340c7a83f2702
aaron_tetracube <aaronpsam@gmail.com>
2021-03-27 16:35:23 +0530

    added

--- equality.js
+++ equality.js
@@ -1,6 +1,8 @@
 const age = "18";
-if(age === 18) // use this equality. here 18 string is not converted to number 18 , in cse of Strings only
+if(age === 18) // use this equality. here 18 string is not converted to number 18 , in case of Strings only
     console.log("You are an Adult (strict)");
-if(age == 18)  //here 18 string is converted to number 18 and is compared with number 18, in cse of Strings only
+if(age == 18)  //here 18 string is converted to number 18 and is compared with number 18, in case of Strings only
     console.log("You are an Adult (loose)");    
 const fav = prompt("What's your favourtie number");    
+console.log(fav);
+console.log(typeof(fav));
