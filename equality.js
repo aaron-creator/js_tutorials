89498c7b4081ebfc2fc702b665a43a111f08752b
aaron_tetracube <aaronpsam@gmail.com>
2021-03-27 11:48:21 +0530

    updated

--- equality.js
+++ equality.js
@@ -1,5 +1,5 @@
 const age = "18";
+if(age === 18)
+    console.log("You are an Adult (strict)");
 if(age == 18)
-    console.log("You are an Adult");
-else
-    console.log("You are not an Adult");    
+    console.log("You are an Adult (loose)");    
