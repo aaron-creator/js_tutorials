6bdb0a3def60531d8b0aaedd83e03a329d6a4573
aaron_tetracube <aaronpsam@gmail.com>
2021-03-27 16:40:46 +0530

    updated

--- equality.js
+++ equality.js
@@ -3,6 +3,9 @@ if(age === 18) // use this equality. here 18 string is not converted to number 1
     console.log("You are an Adult (strict)");
 if(age == 18)  //here 18 string is converted to number 18 and is compared with number 18, in case of Strings only
     console.log("You are an Adult (loose)");    
-const fav = prompt("What's your favourtie number");    
+const fav = Number(prompt("What's your favourite number"));   
 console.log(fav);
 console.log(typeof(fav));
+
+if(fav === 23)
+    console.log(`Cool! ${fav} is an ammazing number`);
