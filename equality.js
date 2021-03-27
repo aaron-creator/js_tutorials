dbc0acc716fe7d7167157b41ee69d59d32705c66
aaron_tetracube <aaronpsam@gmail.com>
2021-03-27 16:26:25 +0530

    added

--- equality.js
+++ equality.js
@@ -3,4 +3,4 @@ if(age === 18) // use this equality. here 18 string is not converted to number 1
     console.log("You are an Adult (strict)");
 if(age == 18)  //here 18 string is converted to number 18 and is compared with number 18, in cse of Strings only
     console.log("You are an Adult (loose)");    
-prompt("What's your favourtie color");    
+const fav = prompt("What's your favourtie number");    
