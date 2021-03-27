807568ee8b27c6a9d88b622b52a02b9e6d26a35b
aaron_tetracube <aaronpsam@gmail.com>
2021-03-27 11:54:46 +0530

    comments added.

--- equality.js
+++ equality.js
@@ -1,5 +1,5 @@
 const age = "18";
-if(age === 18)
+if(age === 18) //here 18 string is not converted to number 18 , in cse of Strings only
     console.log("You are an Adult (strict)");
-if(age == 18)
+if(age == 18)  //here 18 string is converted to number 18 and is compared with number 18, in cse of Strings only
     console.log("You are an Adult (loose)");    
