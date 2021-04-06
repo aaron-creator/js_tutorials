40b07101b5a5e371cedc16364436fe05e20feeff
aaron_tetracube <aaronpsam@gmail.com>
2021-04-06 23:03:01 +0530

    added

--- logical.js
+++ logical.js
@@ -12,4 +12,4 @@ else
     console.log("Someone else should drive..");
 
 const isTired = true;
-console.log(hasDriversLicense || hasGoodVision || isTired);
+console.log(hasDriversLicense && hasGoodVision && isTired);
